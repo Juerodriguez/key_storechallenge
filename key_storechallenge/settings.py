ca5668086ac1615f03c7b8351e6fa43ea98aecf2
Juan Elias Rodriguez <stylorj@gmail.com>
2023-02-17 23:27:28 -0300

    settings disable auto slash APPEND_SLASH = False

--- key_storechallenge/settings.py
+++ key_storechallenge/settings.py
@@ -146,7 +146,10 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.ScryptPasswordHasher',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4000',
+    'http://localhost:4200',
 ]
+
+APPEND_SLASH = False
 
